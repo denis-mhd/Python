82abd13a335e3a0ffb498ffce12fddbbd7bfbfda
Denis Mehmed <54396057+denis-mhd@users.noreply.github.com>
2021-04-01 15:48:54 +0300

    Delete 101-Hello Python!.py

--- 101-Hello Python!.py
+++
@@ -1,7 +0,0 @@
-for c in range(1, 48):
-    for b in range(1, c):
-        for a in range(1, b):
-            if a * a + b * b == c * c:
-                print('{}, {}, {}'.format(a,b,c))
-
-                
